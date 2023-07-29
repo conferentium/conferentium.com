cf6344e5c3a52e5ccf0572e58a7a8819fe41d88d
Sharan Chandran <sharanc25@gmail.com>
2023-07-29 23:00:40 +0530

    Adds home page

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <!DOCTYPE html>
+    <html lang="en" class="light scroll-smooth" dir="ltr">
+    <body class="font-nunito text-base text-black dark:text-white dark:bg-slate-900">
+<!--    <NavCenter/>-->
+    <slot/>
+    </body>
+    </html>
+</template>
+<script setup lang="ts">
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    app: {
+        pageTransition: {name: 'page', mode: 'out-in'}
+    },
+    css: ['~/assets/css/main.css'],
+    head: {
+        script: [
+            {
+                src: "/assets/libs/wow.js/wow.min.js",
+                body: true,
+            },
+            {
+                src: "/assets/libs/tobii/js/tobii.min.js",
+                body: true,
+            },
+            {
+                src: "/assets/libs/tiny-slider/min/tiny-slider.js",
+                body: true,
+            },
+            {
+                src: "/assets/libs/feather-icons/feather.min.js",
+                body: true,
+            },
+            {
+                src: "/assets/js/plugins.init.js",
+                body: true,
+            },
+            {
+                src: "/assets/js/app.js",
+                body: true,
+            },
+        ],
+    },
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 })
